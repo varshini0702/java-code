c557931e2070188d398dd1fe1bb57b9ff87d96d7
varshini0702 <pvarshinil2007@gmail.com>
2026-07-09 22:24:25 +0530

    Time: 5 ms (74.44%), Space: 44.1 MB (90.97%) - LeetHub

---
+++ 1195-fizz-buzz-multithreaded/1195-fizz-buzz-multithreaded.java
@@ -0,0 +1,85 @@
+import java.util.concurrent.Semaphore;
+import java.util.function.IntConsumer;
+
+class FizzBuzz {
+    private int n;
+    private int current = 1;
+
+    private Semaphore fizz = new Semaphore(0);
+    private Semaphore buzz = new Semaphore(0);
+    private Semaphore fizzbuzz = new Semaphore(0);
+    private Semaphore number = new Semaphore(1);
+
+    public FizzBuzz(int n) {
+        this.n = n;
+    }
+
+    private void next() {
+        current++;
+
+        if (current > n) {
+            fizz.release();
+            buzz.release();
+            fizzbuzz.release();
+            number.release();
+            return;
+        }
+
+        if (current % 3 == 0 && current % 5 == 0)
+            fizzbuzz.release();
+        else if (current % 3 == 0)
+            fizz.release();
+        else if (current % 5 == 0)
+            buzz.release();
+        else
+            number.release();
+    }
+
+    public void fizz(Runnable printFizz) throws InterruptedException {
+        while (true) {
+            fizz.acquire();
+
+            if (current > n)
+                return;
+
+            printFizz.run();
+            next();
+        }
+    }
+
+    public void buzz(Runnable printBuzz) throws InterruptedException {
+        while (true) {
+            buzz.acquire();
+
+            if (current > n)
+                return;
+
+            printBuzz.run();
+            next();
+        }
+    }
+
+    public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        while (true) {
+            fizzbuzz.acquire();
+
+            if (current > n)
+                return;
+
+            printFizzBuzz.run();
+            next();
+        }
+    }
+
+    public void number(IntConsumer printNumber) throws InterruptedException {
+        while (true) {
+            number.acquire();
+
+            if (current > n)
+                return;
+
+            printNumber.accept(current);
+            next();
+        }
+    }
+}
